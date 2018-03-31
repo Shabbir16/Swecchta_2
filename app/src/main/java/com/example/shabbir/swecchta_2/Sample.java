ecc4c0fd70fb420a9defb3e41b0dce06bc2d3bc8
shabbir <pshabbir36@gmail.com>
2018-03-31 14:57:21 +0530

    Minor bug fix

---
+++ app/src/main/java/com/example/shabbir/swecchta_2/Sample.java
@@ -0,0 +1,33 @@
+package com.example.shabbir.swecchta_2;
+
+/**
+ * Created by shabbir on 3/28/2018.
+ */
+
+public class Sample {
+    private String a;
+    private String b;
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getA() {
+
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public Sample(String a, String b) {
+
+        this.a = a;
+        this.b = b;
+    }
+}
